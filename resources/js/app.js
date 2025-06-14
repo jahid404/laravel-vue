4053c812bf3a3290f22775efead4186e9ab64844
Jahidul Islam Jahid <jsjahidmini@gmail.com>
2025-06-14 09:51:25 +0600

    Fix: Move App.vue import to components directory
    The App.vue file was moved to the components directory. Update the import path accordingly.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,5 @@
 import './bootstrap';
 import { createApp } from 'vue';
-import App from './App.vue';
 import router from './router';
-
+import App from './components/App.vue';
 createApp(App).use(router).mount('#app')
