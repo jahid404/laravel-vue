7a9fa3985eb67a52abf8b70e06ead051a135907f
Jahidul Islam Jahid <jsjahidmini@gmail.com>
2025-06-14 10:09:25 +0600

    feat: Add About and Contact pages with basic routing
    This commit introduces the About and Contact pages with basic content and configures routing to these pages from the Home page.

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,5 +1,8 @@
 <template>
     <h1 class="text-3xl font-bold">Home Page</h1>
+
+    <router-link to="/about">About</router-link>
+    <router-link to="/contact">Contact</router-link>
 </template>
 
 <script>
