7369f3d01c3d0f835db5af75beba77fd2fa67115
Jahidul Islam Jahid <jsjahidmini@gmail.com>
2025-06-14 09:45:26 +0600

    feat: Integrate Vue.js and configure routing
    This commit integrates Vue.js into the application and configures basic routing.  It imports the necessary Vue modules, including the main App component and router, and mounts the application to the #app element.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,6 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-const app = Vue.createApp({})
+createApp(App).use(router).mount('#app')
