bb81886d211d08656b150843d81a9995f45b8f9a
Jahidul Islam Jahid <jsjahidmini@gmail.com>
2025-06-14 10:11:24 +0600

    feat: Style Home page links using Bootstrap button classes

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -2,9 +2,13 @@
     <div class="flex flex-col items-center justify-center h-screen">
         <h1 class="text-3xl font-bold">Home Page</h1>
 
-    <div class="flex justify-center items-center gap-4">
-        <router-link to="/about" class="bg-blue-400 text-white px-4 py-2 rounded-md">About</router-link>
-            <router-link to="/contact" class="bg-blue-400 text-white px-4 py-2 rounded-md">Contact</router-link>
+        <div class="flex justify-center items-center gap-4 mt-5">
+            <router-link to="/about" class="btn btn-primary"
+                >About
+            </router-link>
+            <router-link to="/contact" class="btn btn-primary"
+                >Contact
+            </router-link>
         </div>
     </div>
 </template>
